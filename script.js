e1cb18a1d39a190f184de4ca1b41d42d98097be8
nicolasraube <raubenicolas@gmail.com>
2017-07-24 13:03:01 +0200

    canvas scaling works

--- script.js
+++ script.js
@@ -27,6 +27,8 @@ class Canvas {
 		this.canvas.style.height = 300 + 'px';
 
 		this.context = this.canvas.getContext('2d');
+
+		this.wrapper = document.getElementById('canvas-wrapper');
 	}
 
 	setResolution(width, height) {
@@ -43,7 +45,46 @@ class Canvas {
 	}
 
 	calculateVisualSize(width, height) {
+		let maxWidth = this.getWrapperWidth();
+		let maxHeight = this.getWrapperHeight();
+
+		if (width === maxWidth) {
+			let factor = maxHeight / height;
+			height = maxHeight;
+			width = width * factor;
+		} else if (height === maxHeight) {
+			let factor = maxWidth / width;
+			width = maxWidth;
+			height = height * factor;
+		} else	if (maxWidth >= maxHeight) {
+			let factor = maxWidth / width;
+			width = maxWidth;
+			height = height * factor;
+
+			if (height > maxHeight) {
+				this.calculateVisualSize(width, height);
+				return;
+			}
+		} else {
+			let factor = maxHeight / height;
+			height = maxHeight;
+			width = width * factor;
+
+			if (width > maxWidth) {
+				this.calculateVisualSize(width, height);
+				return;
+			}
+		}
+
 		this.setVisualSize(width, height);
+	}
+
+	getWrapperWidth() {
+		return this.wrapper.getBoundingClientRect().width;
+	}
+
+	getWrapperHeight() {
+		return this.wrapper.getBoundingClientRect().height;
 	}
 
 	setVisualSize(width, height) {
